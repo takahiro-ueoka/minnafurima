6f73371f1aff114c94731bd58492b0318e70a1af
David <dilots@gmail.com>
2013-07-27 17:24:52 +0900

    Added new databases.

--- server/publish.js
+++ server/publish.js
@@ -1,22 +1,41 @@
 // Lists -- {name: String}
 Lists = new Meteor.Collection("lists");
-
 // Publish complete set of lists to all clients.
 Meteor.publish('lists', function () {
   return Lists.find();
 });
 
+// Items -- {id: Number
+//			name: String,
+//			isForSale: Boolean
+//			category: String
+//			description: String
+//			clientName: String
+//			clientAddress: String
+//			price: Number
+//			registrationDate: DateTime
+//			removalDate: DateTime
+//			isActive: Boolean
+Items = new Meteor.Collection("items");
 
-// products -- {text: String,
-//           done: Boolean,
-//           tags: [String, ...],
-//           list_id: String,
-//           timestamp: Number}
-products = new Meteor.Collection("products");
+// Items -- {id: Number
+//			itemId: Number,
+//			Candidate dateTimeList: String (list of DateTimes)
+//			appointmentDate: DateTime
+//			clientName: String
+//			clientAddress: String
+Appointments = new Meteor.Collection("appointments");
 
-// Publish all items for requested list_id.
-Meteor.publish('products', function (list_id) {
-  check(list_id, String);
-  return products.find({list_id: list_id});
+// Publish all items for requested id.
+Meteor.publish('items', function (id) {
+  check(id, Number);
+  return products.find({id: id});
 });
+
+// Publish all appointments for requested id.
+Meteor.publish('appointments', function (id) {
+  check(id, Number);
+  return products.find({id: id});
+});
+
 
